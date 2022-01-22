1a23d7c5850999baa987b8f5c6a9e30a0f28b8cf
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2022-01-22 20:03:54 +0100

    Building game file and adding loop functionality

---
+++ src/Loop.ts
@@ -0,0 +1,28 @@
+class Loop {
+  private update: () => void;
+  private draw: () => void;
+  private isRunning: boolean;
+
+  constructor(update: () => void, draw: () => void) {
+    this.update = update;
+    this.draw = draw;
+    this.isRunning = false;
+  }
+
+  start() {
+    this.isRunning = true;
+    this.step();
+  }
+
+  stop() {
+    this.isRunning = false;
+  }
+
+  private step() {
+    if (!this.isRunning) return;
+
+    this.update();
+    this.draw();
+    requestAnimationFrame(() => this.step());
+  }
+}
